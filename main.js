7eacacc551149a5d180a7621faae9b607617441d
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-09-26 17:56:49 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// the + at the start of the assignment
+// converts the strings to numbers
+// such that 5 + 5 = 10 instead of 55
+// (parseFloat or parseInt would work too)
+var number1 = +window.prompt('First Number')
+var number2 = +window.prompt('Second Number')
+var number3 = +window.prompt('Third Number')
+
+alert(number1 + number2 + number3)
